f4331cb179ef9ca14595861f0b006ccd2fcfccd5
Freya Gao <fgao30@student.monash.edu>
2017-04-29 20:52:49 +1000

    Modify GUI class and driver class
    Attempted to initiate GUI window to show when main function from
    TestWeatherService is run. Not yet functioning correctly: error shown
    for Mainframe.<init>.

--- Weather Monitor Application/src/MainFrame.java
+++ Weather Monitor Application/src/MainFrame.java
@@ -1,14 +1,18 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 
 /**
  * Created by Freya on 27/04/2017.
  */
-public class MainFrame {
+public class MainFrame extends JFrame{
+    private JFrame frame = new JFrame("WeatherApp");
     private JList locationListView;
-    private JPanel mainPanel;
+    private DefaultListModel model;
+    public JPanel mainPanel;    // TODO: check later
     private JButton fetchLocationListBtn;
     private JCheckBox showTemperatureCheckBox;
     private JCheckBox showRainfallCheckBox;
@@ -16,29 +20,45 @@ public class MainFrame {
     private JButton displayBtn;
     // private ArrayList<Location> locationList;
 
-    public void main(String[] args) {
-
-        // Creating and opening the main frame
-        JFrame frame = new JFrame("WeatherApp");    // Define JFrame
-        frame.setContentPane(new MainFrame().mainPanel);     // Call panel inside frame
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-
+    public MainFrame(){
+        System.out.print("ok");
+        initialise();
         fetchLocationListBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Retrieve list of locations and display them in locationListView.
                 // TODO:
+                // JList needs a model to load values
+                DefaultListModel model = new DefaultListModel();
+                model.addElement("Jane Doe");
+                locationListView.setModel(model);
             }
         });
 
         displayBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Create a new window to display information of selecter location.
+                // Create a new window to display information of selected location.
             }
         });
+    }
+
+//    public static void main(String[] args) {
+//
+//    }
+
+    public void initialise() {
+        // Creating and opening the main frame
+        // JFrame frame = new JFrame("WeatherApp");    // Define JFrame
+        frame.setContentPane(new MainFrame().mainPanel);     // Call panel inside frame
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        frame.pack();
+        frame.setVisible(true);
     }
 
     private void createUIComponents() {
--- Weather Monitor Application/src/TestWeatherService.java
+++ Weather Monitor Application/src/TestWeatherService.java
@@ -1,8 +1,13 @@
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.lang.Exception;
 
 import MelbourneWeather2.MelbourneWeather2Stub;
 import MelbourneWeather2.MelbourneWeather2Stub.*;
+
+import javax.swing.*;
 
 /*
  * Basic example of use of MelbourneWeather2 Axis2 web services in Java
@@ -27,27 +32,45 @@ public class TestWeatherService {
 		// Get the available locations from the web service
 		GetLocationsResponse LocationsResponse = MelbourneWeatherService.getLocations();
 		String[] Locations = LocationsResponse.get_return();
-		
-		// Loop over the locations, and display the temperature and rainfall at each
-		for (int i = 0; i < Locations.length; i++) {
-			// Get rainfall
-			GetRainfall RainfallRequest = new GetRainfall();
-			RainfallRequest.setLocation(Locations[i]);
-			GetRainfallResponse RainfallResponse = MelbourneWeatherService.getRainfall(RainfallRequest);
-			String[] Rainfall = RainfallResponse.get_return();
-			// Get temperature
-			GetTemperature TemperatureRequest = new GetTemperature();
-			TemperatureRequest.setLocation(Locations[i]);
-			GetTemperatureResponse TemperatureResponse = MelbourneWeatherService.getTemperature(TemperatureRequest);
-			String[] Temperature = TemperatureResponse.get_return();
-			System.out.print(
-				Locations[i]
-				+ " @ " + Rainfall[TimestampIndex]
-				+ ":\n\tTemperature:\t" + Temperature[TemperatureIndex]
-				+ "\n\tRainfall:\t" + Rainfall[RainfallIndex]
-				+ "\n\n"
-			);
-		}	
+
+
+//		// Creating and opening the main frame
+//		JFrame frame = new JFrame();    // Define JFrame
+//		System.out.print("ok");
+//		frame.setContentPane(new MainFrame().mainPanel);     // Call panel inside frame
+//		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//		frame.addWindowListener(new WindowAdapter() {
+//			public void windowClosing(WindowEvent e) {
+//				System.exit(0);
+//			}
+//		});
+//		frame.pack();
+//		frame.setVisible(true);
+
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
+
+
+//		// Loop over the locations, and display the temperature and rainfall at each
+//		for (int i = 0; i < Locations.length; i++) {
+//			// Get rainfall
+//			GetRainfall RainfallRequest = new GetRainfall();
+//			RainfallRequest.setLocation(Locations[i]);
+//			GetRainfallResponse RainfallResponse = MelbourneWeatherService.getRainfall(RainfallRequest);
+//			String[] Rainfall = RainfallResponse.get_return();
+//			// Get temperature
+//			GetTemperature TemperatureRequest = new GetTemperature();
+//			TemperatureRequest.setLocation(Locations[i]);
+//			GetTemperatureResponse TemperatureResponse = MelbourneWeatherService.getTemperature(TemperatureRequest);
+//			String[] Temperature = TemperatureResponse.get_return();
+//			System.out.print(
+//				Locations[i]
+//				+ " @ " + Rainfall[TimestampIndex]
+//				+ ":\n\tTemperature:\t" + Temperature[TemperatureIndex]
+//				+ "\n\tRainfall:\t" + Rainfall[RainfallIndex]
+//				+ "\n\n"
+//			);
+//		}
 	}
 
 }
