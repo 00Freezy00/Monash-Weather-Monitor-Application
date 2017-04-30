19a2cc5b2fcebf56d1433474dd87a0830dfb1c3d
Freezy <jackzhang0096@gmail.com>
2017-04-30 23:41:28 +1000

    Implement a fix for bug

--- Weather Monitor Application/src/WeatherGrabber.java
+++ Weather Monitor Application/src/WeatherGrabber.java
@@ -4,7 +4,6 @@
  * Author: Yi Fei (Freya) Gao, Yun Hao (Jack) Zhang
  */
 abstract class WeatherGrabber implements Runnable{
-    public abstract Observer newLocationObserver(String location)throws Exception;
     public abstract String[] grabRainFall(String location) throws Exception;
     public abstract String[] grabTemperature(String location) throws Exception;
     public abstract String[] grabLocations() throws Exception;
