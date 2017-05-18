4bed68c698931fe91a668233cebf67f4650c1465
00Freezy00 <jackzhang0096@gmail.com>
2017-05-18 23:26:12 +1000

    Escalated Monitor adapter as a interface

--- Weather Monitor Application/src/MonitorAdapter.java
+++ Weather Monitor Application/src/MonitorAdapter.java
@@ -1,65 +1,10 @@
-import javax.swing.*;
-import java.awt.*;
-
 /**
- * Created by Jack on 30-Apr-17.
+ * Created by Jack on 18/5/17.
  */
-public class MonitorAdapter {
-    private WeatherFrame weatherFrame;
-    private boolean[] displayMode;
-
-
-
-    private LocationObserver locationObserver;
-
-    public MonitorAdapter(boolean[]displayMode,String location){
-        this.displayMode = displayMode;
-        weatherFrame = new WeatherFrame("Monitor",this,location);
-        weatherFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        weatherFrame.pack();
-        displayOption();
-        weatherFrame.setVisible(true);
-    }
-
-    public void displayTemperature(String temperature){
-        try{
-            Double.parseDouble(temperature);
-        }
-        catch(NumberFormatException ex){
-            temperature = "-";
-        }
-        weatherFrame.setTemperatureLabel(temperature+"°C");
-    }
-
-    public void displayRainFall(String rainFall){
-        try{
-            Double.parseDouble(rainFall);
-        }
-        catch(NumberFormatException ex){
-            rainFall = "-";
-        }
-        weatherFrame.setRainfallLabel(rainFall+"mm");
-    }
-
-    public void displayLastUpdated(String timeStamp){
-        weatherFrame.setLastUpdated(timeStamp);
-    }
-
-    private void displayOption(){
-        if (!displayMode[0]){
-            weatherFrame.disableTemperatureData();
-        }
-        if (!displayMode[1]){
-            weatherFrame.disableRainData();
-        }
-    }
-
-    public void disposeMyself(){
-        this.locationObserver.removeMonitorAdapter(this);
-    }
-
-    public void setLocationObserver(LocationObserver locationObserver) {
-        this.locationObserver = locationObserver;
-    }
-
+public interface MonitorAdapter {
+    void displayTemperature(String temperature);
+    void displayRainFall(String rainFall);
+    void disposeMyself();
+    void displayLastUpdated(String timeStamp);
+    void setLocationObserver(LocationObserver locationObserver);
 }
---
+++ Weather Monitor Application/src/NormalDisplay.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Jack on 30-Apr-17.
+ */
+public class NormalDisplay implements MonitorAdapter {
+    private WeatherFrame weatherFrame;
+    private boolean[] displayMode;
+
+
+
+    private LocationObserver locationObserver;
+
+    public NormalDisplay(boolean[]displayMode,String location){
+        this.displayMode = displayMode;
+        weatherFrame = new WeatherFrame("Monitor",this,location);
+
+        weatherFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        displayOption();
+        weatherFrame.pack();
+        weatherFrame.setVisible(true);
+    }
+
+    public void displayTemperature(String temperature){
+        try{
+            Double.parseDouble(temperature);
+        }
+        catch(NumberFormatException ex){
+            temperature = "-";
+        }
+        weatherFrame.setTemperatureLabel(temperature+"°C");
+    }
+
+    public void displayRainFall(String rainFall){
+        try{
+            Double.parseDouble(rainFall);
+        }
+        catch(NumberFormatException ex){
+            rainFall = "-";
+        }
+        weatherFrame.setRainfallLabel(rainFall+"mm");
+    }
+
+    public void displayLastUpdated(String timeStamp){
+        weatherFrame.setLastUpdated(timeStamp);
+    }
+
+    private void displayOption(){
+        if (!displayMode[0]){
+            weatherFrame.disableTemperatureData();
+        }
+        if (!displayMode[1]){
+            weatherFrame.disableRainData();
+        }
+    }
+
+    public void disposeMyself(){
+        this.locationObserver.removeMonitorAdapter(this);
+    }
+
+    public void setLocationObserver(LocationObserver locationObserver) {
+        this.locationObserver = locationObserver;
+    }
+
+}
