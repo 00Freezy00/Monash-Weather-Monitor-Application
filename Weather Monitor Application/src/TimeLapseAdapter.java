38f8aac9a33b48b9a9d15f46722bc028975ddc6d
Freya Gao <fgao30@student.monash.edu>
2017-05-21 23:13:08 +1000

    Modify TimeLapseAdapter
    Time lapse option now displays correct updates from the sources.

--- Weather Monitor Application/src/TimeLapseAdapter.java
+++ Weather Monitor Application/src/TimeLapseAdapter.java
@@ -2,11 +2,13 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.labels.StandardXYToolTipGenerator;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.time.Day;
+import org.jfree.data.time.Millisecond;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYDataset;
@@ -17,6 +19,7 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.text.DateFormat;
 import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -31,51 +34,41 @@ public class TimeLapseAdapter implements MonitorAdapter {
     private boolean[] displayMode;
     private TimeSeries temperatureSeries = new TimeSeries("Temperature");
     private TimeSeries rainfallSeries = new TimeSeries("Rainfall");
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
 
     public TimeLapseAdapter(boolean[] displayMode, String location, String source) {
         this.displayMode = displayMode;
         this.location = location;
-
         this.weatherTimeLapseFrame = new WeatherTimeLapseFrame(source + " Time Lapse", this, location);
-
-        temperatureSeries.add(new Day(2, MonthConstants.JANUARY, 2002), 95.565);
-        temperatureSeries.add(new Day(3, MonthConstants.JANUARY, 2002), 95.640);
-        rainfallSeries.add(new Day(2, MonthConstants.JANUARY, 2002), 410);
-        rainfallSeries.add(new Day(3, MonthConstants.JANUARY, 2002), 455);
-
         applyChart();
     }
 
-
-
     @Override
     public void displayTemperature(String temperature, String temperatureTimeStamp) {
-
-    }
-
-    @Override
-    public void displayRainFall(String rainFall, String rainFallTimeStamp) {
-
-
-    // TODO: also pass in timestamp, and convert them from String to Date objects
-
-    @Override
-    /*public void displayTemperature(String temperature) {
         if (displayMode[0]) {
             // Add new temperature to series
-            float t = Float.parseFloat(temperature);
-            temperatureSeries.add(new Day(4, MonthConstants.JANUARY, 2002), t);
+            addToSeries(this.temperatureSeries, temperature, temperatureTimeStamp);
         }
     }
 
     @Override
-    public void displayRainFall(String rainFall) {
+    public void displayRainFall(String rainFall, String rainFallTimeStamp) {
         if (displayMode[1]) {
             // Add new rainfall to series
-            float r = Float.parseFloat(rainFall);
-            rainfallSeries.add(new Day(4, MonthConstants.JANUARY, 2002), r);
-        }*/
+            addToSeries(this.rainfallSeries, rainFall, rainFallTimeStamp);
+        }
+    }
 
+    // Modifies graph series to update graph
+    private void addToSeries(TimeSeries series, String value, String timestamp) {
+        try {
+//            System.out.print(timestamp + "\n");
+            Date date = dateFormat.parse(timestamp);
+            float v = Float.parseFloat(value);
+            series.addOrUpdate(new Millisecond(date), v);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -87,8 +80,7 @@ public class TimeLapseAdapter implements MonitorAdapter {
 
     private void displayRetrievalTime() {
         Date date = new Date();
-        DateFormat DateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        weatherTimeLapseFrame.setLastUpdated(DateFormat.format(date));
+        weatherTimeLapseFrame.setLastUpdated(dateFormat.format(date));
     }
 
     @Override
@@ -100,10 +92,10 @@ public class TimeLapseAdapter implements MonitorAdapter {
     // jFreeChart implementation
 
     /**
-     * Creates a chart.
-     *
-     * @return a chart.
+     * JFreeChart referenced from:
+     * https://github.com/ngadde/playground/blob/master/com.iis.sample1/src/main/java/demo/PriceVolumeDemo1.java
      */
+
     // Removed "static"
     private JFreeChart createChart() {
         String title = "Weather Information for " + this.location;
@@ -112,10 +104,9 @@ public class TimeLapseAdapter implements MonitorAdapter {
 
         if (displayMode[0]) {
             data1 = new TimeSeriesCollection(temperatureSeries);
-            value1 = "Temperature";
         } else {
             data1 = new TimeSeriesCollection(this.rainfallSeries);
-            value1 = "Rainfall";
+            value1 = "Rainfall (mm)";
         }
 
         JFreeChart chart = ChartFactory.createTimeSeriesChart(
@@ -130,25 +121,32 @@ public class TimeLapseAdapter implements MonitorAdapter {
         ChartUtilities.applyCurrentTheme(chart);
 
         XYPlot plot = (XYPlot) chart.getPlot();
+        // Set date axis display format
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        axis.setDateFormatOverride(new SimpleDateFormat("dd/MM/yyyy hh:mm:ss"));
 
         NumberAxis rangeAxis1 = (NumberAxis) plot.getRangeAxis();
         rangeAxis1.setAutoRangeIncludesZero(false);
-        rangeAxis1.setLowerMargin(0.40);
+        // Leave space for other plot
+        rangeAxis1.setUpperMargin(0.20);
+        rangeAxis1.setLowerMargin(0.80);
         DecimalFormat format = new DecimalFormat("00.00");
         rangeAxis1.setNumberFormatOverride(format);
 
-        Shape shape = new Ellipse2D.Double(-4.0, -4.0, 8.0, 8.0);
+        Shape shape = new Ellipse2D.Double(-1.0, -1.0, 2.0, 2.0);
         XYLineAndShapeRenderer renderer1 = (XYLineAndShapeRenderer)
                 plot.getRenderer();
         renderer1.setBaseShapesVisible(true);
         renderer1.setSeriesShape(0, shape);
-        renderer1.setBaseToolTipGenerator(new StandardXYToolTipGenerator(
-                StandardXYToolTipGenerator.DEFAULT_TOOL_TIP_FORMAT,
-                new SimpleDateFormat("d-mm-yyyy"), new DecimalFormat("0.00")));
+//        renderer1.setBaseToolTipGenerator(new StandardXYToolTipGenerator(
+//                StandardXYToolTipGenerator.DEFAULT_TOOL_TIP_FORMAT,
+//                new SimpleDateFormat("d-mm-yyyy"), new DecimalFormat("0.00")));
 
         if (displayMode[0] && displayMode[1]) {  // second dataset only created when both temperature and rainfall selected
-            NumberAxis rangeAxis2 = new NumberAxis("Rainfall");
-            rangeAxis2.setUpperMargin(1.00);  // to leave room for temperature plot
+            NumberAxis rangeAxis2 = new NumberAxis("Rainfall (mm)");
+            // Leave room for temperature plot
+            rangeAxis2.setUpperMargin(1.00);
+            rangeAxis1.setLowerMargin(0.20);
             plot.setRangeAxis(1, rangeAxis2);
             XYDataset data2 = new TimeSeriesCollection(this.rainfallSeries);
             plot.setDataset(1, data2);
@@ -157,11 +155,6 @@ public class TimeLapseAdapter implements MonitorAdapter {
             XYLineAndShapeRenderer renderer2 = new XYLineAndShapeRenderer();
             renderer2.setSeriesShape(0, shape);
             renderer2.setSeriesPaint(0, Color.BLUE);
-            renderer2.setBaseToolTipGenerator(
-                    new StandardXYToolTipGenerator(
-                            StandardXYToolTipGenerator.DEFAULT_TOOL_TIP_FORMAT,
-                            new SimpleDateFormat("d-mm-yyyy"),
-                            new DecimalFormat("0.00")));
             plot.setRenderer(1, renderer2);
         }
         ChartUtilities.applyCurrentTheme(chart);
