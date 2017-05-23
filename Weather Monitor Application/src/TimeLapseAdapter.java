c7438fb106273185b2c7396b26deaf7c0725b639
Freya Gao <fgao30@student.monash.edu>
2017-05-24 00:34:58 +1000

    Clean up and add comments for TimeLapseAdapter

--- Weather Monitor Application/src/TimeLapseAdapter.java
+++ Weather Monitor Application/src/TimeLapseAdapter.java
@@ -4,29 +4,31 @@ import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.labels.StandardXYToolTipGenerator;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.time.Day;
 import org.jfree.data.time.Millisecond;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYDataset;
-import org.jfree.date.MonthConstants;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
-import java.text.DateFormat;
 import java.text.DecimalFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * Referenced from:
- * https://github.com/ngadde/playground/blob/master/com.iis.sample1/src/main/java/demo/PriceVolumeDemo1.java#L19
+ * TimeLapseAdapter.java
+ * An adapter class which translates data from LocationObserver.java and passes it to WeatherTimeLapseFrame
+ * to display a graph view.
+ *
+ * JFreeChart related functions referenced from official JFreeChart website:
+ * https://github.com/ngadde/playground/blob/master/com.iis.sample1/src/main/java/demo/PriceVolumeDemo1.java
+ *
+ * Author: Yifei (Freya) Gao, Yun Hao (Jack) Zhang
  */
+
 public class TimeLapseAdapter implements MonitorAdapter {
     private WeatherTimeLapseFrame weatherTimeLapseFrame;
     private LocationObserver locationObserver;
@@ -34,8 +36,14 @@ public class TimeLapseAdapter implements MonitorAdapter {
     private boolean[] displayMode;
     private TimeSeries temperatureSeries = new TimeSeries("Temperature");
     private TimeSeries rainfallSeries = new TimeSeries("Rainfall");
-    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy H:mm:ss");
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy H:mm:ss");
 
+    /**
+     * An init function that constructs a TimeLapseAdapter
+     * @param displayMode A boolean array which represents user's selection. [0] represents temperature, [1] represents rainfall
+     * @param location A string representing location name
+     * @param source A string representing the data source it comes from
+     */
     public TimeLapseAdapter(boolean[] displayMode, String location, String source) {
         this.displayMode = displayMode;
         this.location = location;
@@ -43,6 +51,11 @@ public class TimeLapseAdapter implements MonitorAdapter {
         applyChart();
     }
 
+    /**
+     * Called to add an entry of temperature into graph
+     * @param temperature String representing temperature in degree C
+     * @param temperatureTimeStamp String containing the timestamp of temperature update
+     */
     @Override
     public void displayTemperature(String temperature, String temperatureTimeStamp) {
         if (displayMode[0]) {
@@ -51,6 +64,11 @@ public class TimeLapseAdapter implements MonitorAdapter {
         }
     }
 
+    /**
+     * Called to add an entry of rainfall into graph
+     * @param rainFall String representing rainfall in mm
+     * @param rainFallTimeStamp String containing the timestamp of rainfall update
+     */
     @Override
     public void displayRainFall(String rainFall, String rainFallTimeStamp) {
         if (displayMode[1]) {
@@ -60,21 +78,33 @@ public class TimeLapseAdapter implements MonitorAdapter {
     }
 
     // Modifies graph series to update graph
+
+    /**
+     * Adds the given value and timestamp to the series of the graph
+     * @param series TimeSeries for temperature or rainfall
+     * @param value String representing value
+     * @param timestamp String representing timestamp of given value
+     */
     private void addToSeries(TimeSeries series, String value, String timestamp) {
         try {
             Date date = dateFormat.parse(timestamp);
-            System.out.print(date.toString() + "\n");   // TODO: test
             float v = Float.parseFloat(value);
             series.addOrUpdate(new Millisecond(date), v);
         } catch (Exception e) {
         }
     }
 
+    /**
+     * A method provided to WeatherTimeLapseFrame to close the adapter
+     */
     @Override
     public void disposeMyself() {
         this.locationObserver.removeMonitorAdapter(this);
     }
 
+    /**
+     * Sets the location observer for this adapter
+     */
     @Override
     public void setLocationObserver(LocationObserver locationObserver) {
         this.locationObserver = locationObserver;
@@ -84,16 +114,17 @@ public class TimeLapseAdapter implements MonitorAdapter {
     // jFreeChart implementation
 
     /**
+     * A method to create a JFreeChart chart according to user's selection on temperature/rainfall
+     *
      * JFreeChart referenced from:
      * https://github.com/ngadde/playground/blob/master/com.iis.sample1/src/main/java/demo/PriceVolumeDemo1.java
      */
-
-    // Removed "static"
     private JFreeChart createChart() {
         String title = "Weather Information for " + this.location;
         String value1;
         XYDataset data1;
 
+        // Change axis series and label depending on user's selection.
         if (displayMode[0]) {
             data1 = new TimeSeriesCollection(temperatureSeries);
             value1 = "Temperature (C)";
@@ -125,22 +156,21 @@ public class TimeLapseAdapter implements MonitorAdapter {
         rangeAxis1.setLowerMargin(0.80);
         DecimalFormat format = new DecimalFormat("0.00");
         rangeAxis1.setNumberFormatOverride(format);
-
+        // Apply shape to plots for better visibility
         Shape shape = new Ellipse2D.Double(-1.0, -1.0, 2.0, 2.0);
         XYLineAndShapeRenderer renderer1 = (XYLineAndShapeRenderer)
                 plot.getRenderer();
         renderer1.setBaseShapesVisible(true);
         renderer1.setSeriesShape(0, shape);
-//        renderer1.setBaseToolTipGenerator(new StandardXYToolTipGenerator(
-//                StandardXYToolTipGenerator.DEFAULT_TOOL_TIP_FORMAT,
-//                new SimpleDateFormat("d-mm-yyyy"), new DecimalFormat("0.00")));
 
-        if (displayMode[0] && displayMode[1]) {  // second dataset only created when both temperature and rainfall selected
+        // second dataset only created when both temperature and rainfall selected
+        if (displayMode[0] && displayMode[1]) {
             NumberAxis rangeAxis2 = new NumberAxis("Rainfall (mm)");
             // Leave room for temperature plot
             rangeAxis2.setUpperMargin(1.00);
             rangeAxis1.setLowerMargin(0.20);
             plot.setRangeAxis(1, rangeAxis2);
+            // Assign the axis a series
             XYDataset data2 = new TimeSeriesCollection(this.rainfallSeries);
             plot.setDataset(1, data2);
             plot.setRangeAxis(1, rangeAxis2);
